a18b630ba11e34410a80e8fec1925808e1317196
yashkhopkar3 <ykhopkar3@gmail.com>
2023-02-04 11:06:31 +0530

    Speaking-Technology-App

---
+++ app/src/main/java/com/example/speakingtechnology/Object_Detection.java
@@ -0,0 +1,14 @@
+package com.example.speakingtechnology;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Object_Detection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object_detection);
+    }
+}
