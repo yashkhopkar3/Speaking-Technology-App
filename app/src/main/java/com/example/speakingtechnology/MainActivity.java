6f7e280dcf573dc85523d19d21c3f67430bd8047
yashkhopkar3 <ykhopkar3@gmail.com>
2023-02-12 13:22:23 +0530

    Speaking-Technology-App

--- app/src/main/java/com/example/speakingtechnology/MainActivity.java
+++ app/src/main/java/com/example/speakingtechnology/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView=findViewById(R.id.imageView);
+
 
         textToSpeech= new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
