1bb919aa18fadf813b9269d25d4d15a36c063d96
pawandeep <singhpawandeep315@gmail.com>
2023-02-13 15:49:05 +0530

    added object detection feature to it

--- app/src/main/java/com/example/speakingtechnology/Object_Detection.java
+++ app/src/main/java/com/example/speakingtechnology/Object_Detection.java
@@ -1,15 +1,202 @@
 package com.example.speakingtechnology;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.example.speakingtechnology.ml.SsdMobilenetV11Metadata1;
+
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Object_Detection extends AppCompatActivity {
+
+    ImageView imageView;
+    Paint p;
+    Bitmap bitmap;
+    TextureView textureView;
+    CameraManager cameraManager;
+    SsdMobilenetV11Metadata1 model;
+    ImageProcessor imageProcessor;
+    List<String> labels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_object_detection);
 
+        check_permissions();
+
+        try {
+            model = SsdMobilenetV11Metadata1.newInstance(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            labels = FileUtil.loadLabels(this, "labels.txt");
+            Log.d("labels", "labels loaded "+ labels.get(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        imageView = findViewById(R.id.imageView);
+        p = new Paint();
+        imageProcessor = new ImageProcessor.Builder().add(new ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build();
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        textureView = findViewById(R.id.textureView);
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+                open_camera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+            }
+            @Override
+            public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
+                return false;
+            }
+            @Override
+            public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
+                bitmap = textureView.getBitmap();
+                TensorImage image = TensorImage.fromBitmap(bitmap);
+
+                image = imageProcessor.process(image);
+
+                SsdMobilenetV11Metadata1.Outputs outputs = model.process(image);
+
+                float[] locations = outputs.getLocationsAsTensorBuffer().getFloatArray();
+                float[] classes = outputs.getClassesAsTensorBuffer().getFloatArray();
+                float[] scores = outputs.getScoresAsTensorBuffer().getFloatArray();
+                float[] numberOfDetections = outputs.getNumberOfDetectionsAsTensorBuffer().getFloatArray();
+
+                Bitmap mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                Canvas c = new Canvas(mutable);
+
+                int h=bitmap.getHeight(), w=bitmap.getWidth();
+
+                for(int x=0; x<numberOfDetections[0]; x++){
+                    if(scores[x] > 0.6){
+                        Log.d("mssgs", classes[x]+"");
+                        try{
+                            p.setTextSize(90.0f);
+                            c.drawText(labels.get(x), locations[x+1]*w, locations[x]*h, p);
+                        }
+                        catch (NullPointerException e){
+
+                        }
+                        p.setStyle(Paint.Style.STROKE);
+                        p.setStrokeWidth(30.0f);
+                        c.drawRect(new RectF(locations[x+1]*w, locations[x]*h, locations[x+3]*w, locations[x+2]*h), p);
+                    }
+                }
+
+                imageView.setImageBitmap(mutable);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        model.close();
+    }
+
+    @SuppressLint("MissingPermission")
+    void open_camera(){
+        try {
+            cameraManager.openCamera(cameraManager.getCameraIdList()[0], new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice cameraDevice) {
+                    try {
+                        CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                        Surface surface = new Surface(textureView.getSurfaceTexture());
+                        builder.addTarget(surface);
+                        cameraDevice.createCaptureSession(Collections.singletonList(surface), new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                                try {
+                                    cameraCaptureSession.setRepeatingRequest(builder.build(), null, null);
+                                } catch (CameraAccessException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+
+                            }
+                        }, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice cameraDevice, int i) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void check_permissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+            }
+        }
     }
 }
